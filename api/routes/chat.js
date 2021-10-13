6e07383e890ae48cf17747e2e2128b3c1cefdbc9
Archit <architkeshri4@gmail.com>
2021-10-13 19:23:41 +0530

    data modified

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -3,18 +3,22 @@ var router = express.Router();
 
 const data = [
   {
+    id: "1",
     name: "archit",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
+    id: "2",
     name: "oj",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
+    id: "3",
     name: "pk",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
+    id: "4",
     name: "lk",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
