fc6bb480ea36ef29635b68b238ba3c8a68e797fd
Prakhar <prakhar.gupta.1106@gmail.com>
2021-12-26 22:16:39 +0530

    options added in plan date

--- client/src/components/AddEventModal.js
+++ client/src/components/AddEventModal.js
@@ -1,13 +1,13 @@
-import React, {useState} from "react";
-import Modal from "react-modal";
+import React, {useState, useRef} from "react";
+import Popup from 'react-animated-popup';
 import Datetime from 'react-datetime';
 
-export default function({isOpen, onClose, onEventAdded}) {
+export default function({isOpen, onClose, onEventAdded, matchDetails}) {
     const [title, setTitle] = useState("");
     const [start, setStart] = useState(new Date());
     const [end, setEnd] = useState(new Date());
     const [dateId, setDateId] =useState("");
-
+    const matchId = useRef("")
     const onSubmit = (event) => {
         event.preventDefault();
         console.log("in on submit", title,dateId, start,end);
@@ -20,14 +20,18 @@ export default function({isOpen, onClose, onEventAdded}) {
         onClose();
     }
 
+    const print = matchDetails.map((item)=>{
+        return(
+            <option value={item?._id}>{item?.name}</option>
+        )
+    })
+
     return (
-        <Modal isOpen={isOpen} onRequestClose={onClose}>
+        <Popup visible={isOpen} onClose={onClose}>
             <form onSubmit={onSubmit}>
 
-                <input placeholder="Title" value={title} onChange={e=>setTitle(e.target.value)}></input>
-
-                <input placeholder="Date Name" value={dateId} onChange={e=>setDateId(e.target.value)}></input>
-
+                <input placeholder="Title" value={title} onChange={e=>setTitle(e.target.value)} />
+                <select onChange={() => setDateId(matchId?.current?.value)} ref={matchId}>{print}</select>
                 <div>
                     <label>Start Date</label>
                     <Datetime value={start} onChange={date=> setStart(date)}/>
@@ -39,6 +43,6 @@ export default function({isOpen, onClose, onEventAdded}) {
                 </div>
                 <button>Add Date!</button>
             </form>
-        </Modal>
+        </Popup>
     )
 }
